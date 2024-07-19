815a3702176c79861000f74ef84d4b6eb5c62da6
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2024-07-19 16:03:17 +0530

    added portfolio apis

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./dbconfig");
+const port = process.env.PORT || 5000;
+
+const portfolioRoute = require("./routes/portfolioRoute");
+
+app.use(express.json());
+
+app.use("/api/portfolio", portfolioRoute);
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
