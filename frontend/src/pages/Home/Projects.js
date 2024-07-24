b1d359b2dfc5e982927f50410b39d68f55f0eaec
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2024-07-25 01:41:41 +0530

    addded projects

--- frontend/src/pages/Home/Projects.js
+++ frontend/src/pages/Home/Projects.js
@@ -1,38 +1,76 @@
 import data from './data.json';
+import { useState } from 'react';
 
-const Card = ({ image, heading, description, buttonColor }) => (
-  <div className="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
-    <img className="h-40 object-cover rounded-xl" src={image} alt="" />
-    <div className="p-2">
-      <h2 className="font-bold text-lg mb-2">{heading}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
+const Card = ({ image, heading, description1, onClick, link }) => (
+    <div
+        className="w-60 bg-white rounded-t-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl hover:bg-gray-200 cursor-pointer"
+        onClick={onClick}
+    >
+        <img className="h-40 object-cover rounded-t-xl p-0" src={image} alt="" />
+        <div className="p-2">
+            <h2 className="font-bold text-lg mb-2">{heading}</h2>
+            <p className="text-sm text-gray-600">{description1}</p>
+        </div>
+        <div className="m-2">
+            <a
+                role="button"
+                href={{link}}
+                className="text-white px-3 py-1 block w-20 text-center hover:bg-purple-900 rounded-md bg-purple-700"
+            >
+                Visit
+            </a>
+        </div>
     </div>
-    <div className="m-2">
-      <a role='button' href='#' className={`text-white  px-3 py-1 block w-20 text-center hover:bg-purple-900 rounded-md bg-purple-700`}>Visit</a>
-    </div>
-  </div>
 );
 
 const Projects = () => {
-  return (
-    < div className='bg-slate-300 w-full min-h-screen'>
-    <h1 className='font-bold text-center text-blue-800 text-5xl py-10'>Projects</h1>
-    <div className=" gap-4 flex-wrap flex justify-center items-center py-5 my-2">
-        
-      {data.map((item, index) => (
-        <Card 
-          key={index}
-          image={item.image}
-          heading={item.heading}
-          description={item.description.length > 80 ?
-            `${item.description.substring(0, 80)}...` : item.description
-          }
-          
-        />
-      ))}
-    </div>
-    </div>
-  );
+    const [selectedElement, setSelectedElement] = useState(data[0]);
+
+    return (
+        <div className="bg-slate-300 w-full h-screen flex">
+            <div className="flex flex-col w-1/2 sm:w-full sm:mt-10 sm:text-center justify-center items-center px-10">
+                {selectedElement && (
+                    <div className="text-gray-800">
+                        <img className="h-44 object-cover mb-8 mx-auto p-0" src={selectedElement.image} alt="" />
+                        <h1 className="text-3xl font-bold">{selectedElement.heading}</h1>
+                    
+                        <p className="text-sm mt-10">{selectedElement.description1}</p>
+                        <p className="text-sm mt-10">{selectedElement.description2}</p>
+                        <div className="mt-10 flex justify-start">
+                                {selectedElement.technology.map((tech, index) => (
+                                    <span
+                                        key={index}
+                                        className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                    </div>
+                )}
+            </div>
+
+            <div className="w-1/2 flex flex-col items-center overflow-y-auto no-scrollbar m-12">
+                <div className="flex flex-wrap gap-4 justify-center">
+                    {data.map((item, index) => (
+                        <Card
+                            key={index}
+                            image={item.image}
+                            heading={item.heading}
+                            description1={
+                                item.description1.length > 80
+                                    ? `${item.description.substring(0, 80)}...`
+                                    : item.description1
+                            }
+
+                            link={item.link}
+                            onClick={() => setSelectedElement(item)}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Projects;
