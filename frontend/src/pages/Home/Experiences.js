76fb283354ce25fd33ffa48287ae0470a396afee
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2024-07-24 21:05:38 +0530

    fixed experiences

--- frontend/src/pages/Home/Experiences.js
+++ frontend/src/pages/Home/Experiences.js
@@ -5,16 +5,16 @@ import {
     VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
+import './Experiences.css'
 
 function Experiences() {
-    const [selectedElement, setSelectedElement] = useState(null);
+    const [selectedElement, setSelectedElement] = useState(timelineElements[0]);
 
     return (
-        <div className="min-h-screen w-screen bg-slate-900 flex justify-center items-center">
-            <div className="flex w-full h-auto items-center sm:flex-col ">
-                <div className="w-1/2 sm:w-full h-screen no-scrollbar overflow-y-auto ">
-                    <VerticalTimeline>
+        <div className="min-h-screen w-screen sm:h-auto  bg-slate-900 ">
+            <div className="flex w-full items-center sm:flex-col">
+                <div className="w-1/2 sm:w-full h-screen sm:h-auto no-scrollbar overflow-y-auto">
+                    <VerticalTimeline className="  scale-90 sm:p-5 sm:scale-75  h-screen">
                         {timelineElements.map((element) => {
                             return (
                                 <VerticalTimelineElement
@@ -22,33 +22,49 @@ function Experiences() {
                                     contentStyle={{
                                         padding: '0.5rem',
                                         boxShadow: '0 0.25em 0.5em 0 rgba(0, 0, 0, 0.25), 0 0.4em 1.25em 0 rgba(0, 0, 0, 0.15)',
-                                     
 
                                     }}
-                                    icon={<img src="https://shorturl.at/4nZfz" alt="icon" />}
-                                    iconStyle={{ background: '#fff', color: '#000', height: 'fit', overflow: 'hidden' }}
+                                    icon={<img src={element.icon} alt="icon" />}
+                                    iconStyle={{ background: '#fff', color: '#000', overflow: 'hidden',   }}
                                     onTimelineElementClick={() => setSelectedElement(element)}
-                                >
-                                    <div>
-                                        <h1 className="pt-1 text-md font-bold text-blue-950">{element.title}</h1>
-                                        <h2 className="text-sm font-semibold text-blue-700">{element.location}</h2>
-                                    </div>
+                                    className="hover:transform hover:scale-105 "
+                                    date={element.date}
+                                    dateClassName="mx-2 text-white "
+                              >
+                                    <h1 className="pt-1 text-lg font-bold text-blue-950">{element.title}</h1>
+                                    <h6 className="text-blue-400 text-xs">{element.location}</h6>
                                 </VerticalTimelineElement>
                             );
                         })}
                     </VerticalTimeline>
                 </div>
-                <div className="flex flex-col w-1/2 sm:w-full sm:mt-8 sm:text-center px-2 gap-10">
+                <div className="flex flex-col w-1/2 sm:w-full sm:mt-10 sm:hidden sm:text-center px-10">
                     {selectedElement && (
-                        <div className="text-white">
-                            <h1 className="text-2xl font-bold">{selectedElement.title}</h1>
-                            <h2 className="text-xl">{selectedElement.location}</h2>
-                            <p className="text-sm">{selectedElement.description}</p>
+                        <div className="text-white p-5">
+
+                            <h1 className="text-3xl font-bold">{selectedElement.title}</h1>
+                            <h2 className="text-md">{selectedElement.location}</h2>
+
+                            <p className="text-sm mt-10">{selectedElement.description}</p>
+                            <div className="mt-10 flex justify-start">
+                                {selectedElement.skills.map((skill, index) => (
+                                    <span
+                                        key={index}
+                                        className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300"
+                                    >
+                                        {skill}
+                                    </span>
+                                ))}
+                            </div>
+
+
+
                         </div>
                     )}
                 </div>
+
             </div>
-        </div>
+        </div >
     );
 }
 
--- frontend/src/pages/Home/timelineElements.js
+++ frontend/src/pages/Home/timelineElements.js
@@ -1,73 +1,58 @@
+import { icon } from "@fortawesome/fontawesome-svg-core";
+
 let timelineElements = [
     {
-      id: 1,
-      title: "Software Engineer",
-      location: "Tech Valley, Siliconia",
-      description:
-        "Developing and maintaining web applications, collaborating with cross-functional teams to deliver high-quality software solutions.",
-      buttonText: "View Software Projects",
-      date: "January 2018 - present",
-      icon: "work",
+        id: 1,
+        icon: "https://media.licdn.com/dms/image/C4D0BAQFyEfqyk1IlJg/company-logo_100_100/0/1674052828713/smowcode_logo?e=1729728000&v=beta&t=mjKgZlDAKd-kHWVIEDer2hqxK6_hlpjwO_y2Bu49MKY",
+        title: "SDE",
+        location: "Smowcode, Mumbai",
+        description:
+            "Working as an intern focusing on front-end technologies, using jQuery and jQuery UI to enhance user interfaces.",
+        date: "June 2024 - Present",
+        skills: ["jQuery", "jQuery UI", "CSS", "HTML"]
     },
     {
-      id: 2,
-      title: "Data Scientist",
-      location: "Data City, Numidia",
-      description:
-        "Analyzing complex data sets to derive actionable insights, using statistical techniques and machine learning algorithms.",
-      buttonText: "View Data Projects",
-      date: "March 2015 - December 2017",
-      icon: "work",
+        id: 2,
+        icon: "https://media.licdn.com/dms/image/C4E0BAQHLt2OyRHIppw/company-logo_100_100/0/1630586455186/tedx_spit_logo?e=1729728000&v=beta&t=8qC8j5KCUhffG7GW4mE_TBfqzOMDVMjHFjuRLSwQhc8",
+        title: "Creative Head",
+        location: "TEDx SPIT, Mumbai",
+        description:
+            "Overseeing the creative aspects of TEDx events, including designing promotional materials and managing visual content.",
+        date: "December 2023 - Present",
+        skills: ["Canva", "Adobe Photoshop", "Graphic Design", "Creative Direction"]
     },
     {
-      id: 3,
-      title: "Project Manager",
-      location: "Capital Town, Republica",
-      description:
-        "Overseeing project timelines, ensuring deliverables are met within scope and budget, and managing team communication.",
-      buttonText: "Company Website",
-      date: "July 2012 - February 2015",
-      icon: "work",
+        id: 3,
+        icon:"https://media.licdn.com/dms/image/C4D0BAQFy7g6eUIzYxg/company-logo_100_100/0/1630548118805?e=1729728000&v=beta&t=Ya0Ou0dBb3re1kl42cdiuZPcn0hz9qoNFE8gkISRL4A",
+        title: "Technical Head",
+        location: "Institution's Innovation Council, Sardar Patel Institute of Technology, Mumbai",
+        description:
+            "Developed the official website of the IIC, SPIT to facilitate efficient management of administrative reports, including functionalities for report generation, verification, and analysis.",
+        date: "October 2023 - Present",
+        skills: ["PHP", "MongoDB", "JavaScript", "Web Development"]
     },
     {
-      id: 4,
-      title: "Master's in Computer Science",
-      location: "Innovate University, Educatia",
-      description:
-        "Completed a Master's program focusing on advanced algorithms, data structures, and machine learning techniques.",
-      buttonText: "View Thesis",
-      date: "September 2010 - June 2012",
-      icon: "school",
+        id: 4,
+        icon:"https://media.licdn.com/dms/image/D4D0BAQEdXmlRNpafcA/company-logo_100_100/0/1710093147377?e=1729728000&v=beta&t=xZnzrj6Av_QZHuMcQls3pUGxoMyx28uHndV8418lmis",
+        title: "Head of Marketing",
+        location: "Oculus S.P.I.T",
+        description:
+            "Led a team to gather data on potential sponsors, successfully contacting and securing sponsorships using strategic communication.",
+
+        date: "December 2023 - March 2024",
+        skills: ["Research", "Communication", "Marketing Strategy"]
     },
     {
-      id: 5,
-      title: "Bachelor's in Information Technology",
-      location: "Tech State College, Informatica",
-      description:
-        "Studied various aspects of IT including software development, networking, and database management.",
-      buttonText: "View Projects",
-      date: "September 2006 - June 2010",
-      icon: "school",
-    },
-    {
-      id: 6,
-      title: "Greenwood High School",
-      location: "Greenwood, Naturia",
-      description:
-        "High school education with a focus on science and mathematics, participated in various tech-related extracurricular activities.",
-      date: "September 2002 - June 2006",
-      icon: "school",
-    },
-    {
-      id: 7,
-      title: "Greenwood High School",
-      location: "Greenwood, Naturia",
-      description:
-        "High school education with a focus on science and mathematics, participated in various tech-related extracurricular activities.",
-      date: "September 2002 - June 2006",
-      icon: "school",
-    },
-  ];
-  
-  export default timelineElements;
-  
+        id: 5,
+        icon:"https://media.licdn.com/dms/image/D4D0BAQEdXmlRNpafcA/company-logo_100_100/0/1710093147377?e=1729728000&v=beta&t=xZnzrj6Av_QZHuMcQls3pUGxoMyx28uHndV8418lmis",
+        title: "Head of Marketing",
+        location: "Oculus S.P.I.T",
+        description:
+            "Led a team to gather data on potential sponsors, successfully contacting and securing sponsorships using strategic communication.",
+
+        date: "December 2023 - March 2024",
+        skills: ["Research", "Communication", "Marketing Strategy"]
+    }
+];
+
+export default timelineElements;
